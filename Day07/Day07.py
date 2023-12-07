3cf4fab6a13b50ceb4e4ed91b382615855e55790
Shaun Levy <sd.levy@outlook.com>
2023-12-07 15:20:21 +0000

    Removed old comments and outputs

--- Day07/Day07.py
+++ Day07/Day07.py
@@ -17,7 +17,6 @@ def permute(hand, permutations = []):
             handCopy = [c for c in hand]
             handCopy[index] = card
             permutations.extend(permute(handCopy, [handCopy]))
-            #permutations.extend()
     return permutations
 
 def rank(hand):
@@ -31,16 +30,10 @@ handRanks = [int("".join([f'{r:02d}' for r in rank(hand)])) for hand in hands]
 
 def jScore(hand):
     permutations = []
-    print(hand)
     if "J" in hand:
         hand = [card if card != "J" else "j" for card in hand]
         permutations = permute(hand)
         thisHandScores = [score(permutation) for permutation in permutations]
-        #permutations = [i for _,i in sorted(zip(thisHandScores, permutations))]
-        #thisHandScores = sorted(thisHandScores)
-        for i in range(len(permutations)):
-            print("\t",permutations[i],thisHandScores[i])
-        #input()
         permutations.clear()
         return max(thisHandScores)
     else:
@@ -54,32 +47,25 @@ def score(hand):
         else:
             dictHand[card] += 1
     if len(dictHand) == 1:
-        #print("5 of a kind", end=":")
         return 7
     elif len(dictHand) == 2:
         for card in hand:
             if dictHand[card] == 4:
-                #print("4 of a kind", end=":")
                 return 6
             if dictHand[card] == 3:
-                #print("full house", end=":")
                 return 5
     elif len(dictHand) == 3:
         pairs = 0
         for card in hand:
             if dictHand[card] == 3:
-                #print("3 of a kind", end=":")
                 return 4
             if dictHand[card] == 2:
                 pairs += 1
             if pairs == 2:
-                #print("2 pairs", end=":")
                 return 3
     elif len(dictHand) == 4:
-        #print("1 pair", end=":")
         return 2
     elif len(dictHand) == 5:
-        #print("high card", end=":")
         return 1
 
 handScores = {}
@@ -87,8 +73,6 @@ handScores = {}
 for i, hand in enumerate(hands):
     handScores[i] = jScore(hand)
     #handScores[i] = score(hand)
-
-print(handScores)
 
 indices = list(range(len(hands)))
 indices = [i for _,i in sorted(zip(handScores.values(), indices))][::-1]
@@ -107,16 +91,11 @@ for score in scoreGroups:
     theseIndices = [index for index in scoreGroup]
     theseBids = [bids[index] for index in scoreGroup]
     
-
-
     theseHands = [i for _,i in sorted(zip(theseHandRanks, theseHands))][::-1]
     theseIndices = [i for _,i in sorted(zip(theseHandRanks, theseIndices))][::-1]
     theseBids = [i for _,i in sorted(zip(theseHandRanks, theseBids))][::-1]
 
-
-
     allbids.extend(theseBids[::-1])
 
-print(allbids)
 vals = [bid * (i + 1) for i,bid in enumerate(allbids)]
 print(sum(vals))
