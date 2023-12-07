6f639eea24a7e90da7943670f8148ef13bf4e57a
Shaun Levy <sd.levy@outlook.com>
2023-12-07 12:08:00 +0000

    Day 7 Part 1 done

--- Day07/Day07.py
+++ Day07/Day07.py
@@ -1,9 +1,21 @@
 filename = "inputSmall.txt"
 filename = "inputBig.txt"
 
+cardValues = {str(i):i for i in range(2,10)}
+cardValues["T"] = 10
+cardValues["J"] = 11
+cardValues["Q"] = 12
+cardValues["K"] = 13
+cardValues["A"] = 14
+
+def rank(hand):
+    fullRank = [cardValues[c] for c in hand]
+    return fullRank
+
 data = open(filename).read().split("\n")
 hands = [list(line.split(" ")[0]) for line in data]
 bids = [int(line.split(" ")[1]) for line in data]
+handRanks = [int("".join([f'{r:02d}' for r in rank(hand)])) for hand in hands]
 
 def score(hand):
     dictHand = {}
@@ -54,5 +66,25 @@ scoreGroups = {}
 for score in range(8):
     scoreGroups[score] = [index for index in indices if handScores[index] == score]
 
+allbids = []
+
 for score in scoreGroups:
-    print(score,scoreGroups[score])
+    scoreGroup = scoreGroups[score]
+    theseHands = [hands[index] for index in scoreGroup]
+    theseHandRanks = [handRanks[index] for index in scoreGroup]
+    theseIndices = [index for index in scoreGroup]
+    theseBids = [bids[index] for index in scoreGroup]
+    
+
+
+    theseHands = [i for _,i in sorted(zip(theseHandRanks, theseHands))][::-1]
+    theseIndices = [i for _,i in sorted(zip(theseHandRanks, theseIndices))][::-1]
+    theseBids = [i for _,i in sorted(zip(theseHandRanks, theseBids))][::-1]
+
+
+
+    allbids.extend(theseBids[::-1])
+
+print(allbids)
+vals = [bid * (i + 1) for i,bid in enumerate(allbids)]
+print(sum(vals))
