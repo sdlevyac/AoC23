c4c2cc441dffbb3e9351b3271ee02f85453ac33f
Shaun Levy <sd.levy@outlook.com>
2023-12-11 10:59:54 +0000

    added pairing

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,45 +1,57 @@
-filename = "inputSmall.txt"
-filename = "inputBig.txt"
+length = 6
+num = 100000
+for i in range(100000):
+    length += 5
 
-data = open(filename).read().split("\n")
-data = [list(line) for line in data]
+print(length)
 
-def isEmpty(thisList):
-    return len(thisList) == thisList.count(".")
+#filename = "inputSmall.txt"
+##filename = "inputBig.txt"
 
-row = 0
-while row != len(data):
-    #print(data[row])
-    #input()
-    if isEmpty(data[row]):
-        data.insert(row, [c for c in data[row]])
-        row += 1
-    row += 1
+#data = open(filename).read().split("\n")
+#data = [list(line) for line in data]
 
-col = 0
-while col != len(data[0]):
-    column = [row[col] for row in data]
-    if isEmpty(column):
-        for r in range(len(data)):
-            data[r].insert(col, ".")
-        col += 1
-    col += 1
+#def isEmpty(thisList):
+#    return len(thisList) == thisList.count(".")
 
-coords = {}
-pairs = {}
+#row = 0
+#while row != len(data):
+#    #print(data[row])
+#    #input()
+#    if isEmpty(data[row]):
+#        data.insert(row, [c for c in data[row]])
+#        row += 1
+#    row += 1
 
-count = 1
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        if data[i][j] == "#":
-            label = f'{count:03d}'
-            data[i][j] = label
-            coords[label] = [i,j]
-            count += 1
-        else:
-            data[i][j] = " . "
+#col = 0
+#while col != len(data[0]):
+#    column = [row[col] for row in data]
+#    if isEmpty(column):
+#        for r in range(len(data)):
+#            data[r].insert(col, ".")
+#        col += 1
+#    col += 1
 
-for row in data:
-    print("".join(row))
+#coords = {}
+#pairs = {}
 
-print(coords)
+#count = 1
+#for i in range(len(data)):
+#    for j in range(len(data[0])):
+#        if data[i][j] == "#":
+#            label = f'{count:03d}'
+#            data[i][j] = label
+#            coords[label] = [i,j]
+#            count += 1
+#        else:
+#            data[i][j] = " . "
+
+#for row in data:
+#    print("".join(row))
+
+#for coord in coords:
+#    pairs[coord] = [star for star in coords if star != coord and ((star in pairs and coord not in pairs[star]) or star not in pairs)]
+
+#for pair in pairs:
+#    print(pair, pairs[pair])
+
