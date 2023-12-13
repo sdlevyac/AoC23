af72eaf329ce01355e6265b66ea41375848f830c
Shaun Levy <sd.levy@outlook.com>
2023-12-13 20:55:40 +0000

    Answered Part 2 Code doesn't give the exact answer but by printing all points of uncertainty I could see there were only a few options and manually debugged the answer

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -5,6 +5,57 @@ data = open(filename).read().split("\n\n")
 
 data = [[list(splitLine) for splitLine in line.split("\n")] for line in data]
 
+def findMirror(grid):
+    mirrors = []
+    for r in range(1,len(grid)):
+        tr = r - 1
+        br = r
+        mirror = True
+        while tr >= 0 and br < len(grid) and mirror:
+            if "".join(grid[tr]) != "".join(grid[br]):
+                mirror = False
+            else:
+                tr -= 1
+                br += 1
+        if mirror:
+            mirrors.append(100*r)
+    for r in range(1,len(grid) - 1):
+        tr = r - 1
+        br = r + 1
+        while tr >= 0 and br < len(grid) and mirror:
+            if "".join(grid[tr]) != "".join(grid[br]):
+                mirror = False
+            else:
+                tr -= 1
+                br += 1
+        if mirror:
+            mirrors.append(100*r)
+    for c in range(1,len(grid[0])):
+        lc = c - 1
+        rc = c
+        mirror = True
+        while lc >= 0 and rc < len(grid[0]) and mirror:
+            if "".join([row[lc] for row in grid]) != "".join([row[rc] for row in grid]):
+                mirror = False
+            else:
+                lc -= 1
+                rc += 1
+        if mirror:
+            mirrors.append(c)
+    for c in range(1,len(grid[0]) - 1):
+        lc = c - 1
+        rc = c + 1
+        mirror = True
+        while lc >= 0 and rc < len(grid[0]) and mirror:
+            if "".join([row[lc] for row in grid]) != "".join([row[rc] for row in grid]):
+                mirror = False
+            else:
+                lc -= 1
+                rc += 1
+        if mirror:
+            mirrors.append(c)
+    return mirrors
+
 def draw(grid):
     output = ""
     for row in grid:
@@ -12,91 +63,36 @@ def draw(grid):
             output += col
         output += "\n"
     print(output)
-
-ansR = 0
-ansC = 0
-
-for g,grid in enumerate(data):
-    anyMirror = False
-    for r in range(1,len(grid)):
-        tr = r - 1
-        br = r
-        mirror = True
-        #tr = top row, br = bottom row
-        while tr >= 0 and br < len(grid) and mirror:
-            if "".join(grid[tr]) != "".join(grid[br]):
-                mirror = False
+    
+def smudge(cleanGrid, x, y):
+    smudgedGrid = [["" for c in r] for r in cleanGrid]
+    for i in range(len(cleanGrid)):
+        for j in range(len(cleanGrid[i])):
+            if i == x and j == y:               
+                smudgedGrid[i][j] = "#" if cleanGrid[i][j] == "." else "."
             else:
-                #print(f"row {br} matches row {tr}")
-                tr -= 1
-                br += 1
-        if mirror:
-            print(f"mirrored about y = {r}, for {r} rows")
-            grid.insert(r,["-" for _ in grid[0]])
-            draw(grid)
-            ansR += r
-            anyMirror = True
-    if not anyMirror:
-        for r in range(1,len(grid)):
-            tr = r - 1
-            br = r + 1
-            while tr >= 0 and br < len(grid) and mirror:
-                if "".join(grid[tr]) != "".join(grid[br]):
-                    mirror = False
-                else:
-                    #print(f"row {br} matches row {tr}")
-                    tr -= 1
-                    br += 1
-            if mirror:
-                print(f"mirrored about y = {r}, for {r} rows")
-                grid[r] = ["-" for _ in len(grid[r])]
+                smudgedGrid[i][j] = cleanGrid[i][j]
+    return smudgedGrid
+
+
+for part2 in [False, True]:
+    answer = 0
+    for g, grid in enumerate(data):
+        original = findMirror(grid)[0]
+        if part2:
+            smudgedVals = []
+            found = False
+            for i in range(len(grid)):
+                if found: break
+                for j in range(len(grid[i])):
+                    smudgedGrid = smudge(grid, i, j)
+                    smudged = [val for val in findMirror(smudgedGrid) if val != original]
+                    smudgedVals.extend(smudged)
+            if len(smudgedVals) > 2:
+                print(smudgedVals)
                 draw(grid)
-                ansR += r
-                anyMirror = True
-    if not anyMirror:
-        for c in range(1,len(grid[0])):
-            lc = c - 1
-            rc = c
-            mirror = True
-                #lc = left column, rc = right column
-            while lc >= 0 and rc < len(grid[0]) and mirror:
-                if "".join([row[lc] for row in grid]) != "".join([row[rc] for row in grid]):
-                    mirror = False
-                else:
-                    #print(f"column {lc} matches column {rc}")
-                    lc -= 1
-                    rc += 1
-            if mirror:
-                print(f"mirrored about x = {c}, for {c} columns")
-                ansC += c
-                for r in range(len(grid)):
-                    grid[r].insert(c, "|")
-                draw(grid)
-                anyMirror = True
-    if not anyMirror:
-        for c in range(1,len(grid[0])):
-            lc = c - 1
-            rc = c + 1
-            mirror = True
-                #lc = left column, rc = right column
-            while lc >= 0 and rc < len(grid[0]) and mirror:
-                if "".join([row[lc] for row in grid]) != "".join([row[rc] for row in grid]):
-                    mirror = False
-                else:
-                    #print(f"column {lc} matches column {rc}")
-                    lc -= 1
-                    rc += 1
-            if mirror:
-                print(f"mirrored about x = {c}, for {c} columns")
-                ansC += c
-                for r in range(len(grid)):
-                    grid[r][c] = "|"
-                draw(grid)
-                anyMirror = True
-    if not anyMirror:
-        draw(grid)
-        print(g)
-        input()
-            
-ans = (100*ansR) + ansC
-print(ans)
+            answer += max(smudgedVals)
+        else:
+            answer += original
+           
+    print(answer)
